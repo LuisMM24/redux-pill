0c489c1572bb3ea407e697479530ddf95f95e7b6
Luis Molina <luismateo2416@gmail.com>
2022-04-18 21:20:05 +0200

    register form and login form styles finished

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,17 +1,70 @@
 import { useState } from "react";
+import "./login.scss";
 
 export const Login: React.FC = () => {
   const [isRegistering, setIsRegistering] = useState<boolean>(false);
   return (
-    <section>
-      <form className="loginForm">
-        <label htmlFor="emailInput">Email</label>
-        <input type="text" name="email" id="emailInput" />
+    <main>
+      {!isRegistering && (
+        <section className="formWrapper">
+          <h1>Login</h1>
+          <form className="loginForm">
+            <label htmlFor="emailInput">
+              <p>Email</p>
+              <input type="text" name="email" id="emailInput" />
+            </label>
 
-        <label htmlFor="emailInput">Password</label>
-        <input type="password" name="password" id="passwordInput" />
-      </form>
-      {isRegistering && <form className="registerForm"></form>}
-    </section>
+            <label htmlFor="emailInput">
+              <p>Password</p>
+              <input type="password" name="password" id="passwordInput" />
+            </label>
+            <div className="formButtons">
+              <button type="button" onClick={() => setIsRegistering(true)}>
+                Register
+              </button>
+              <button type="submit" onClick={() => {}}>
+                Login
+              </button>
+            </div>
+          </form>
+        </section>
+      )}
+
+      {isRegistering && (
+        <section className="formWrapper">
+          <h1>Register</h1>
+          <form className="registerForm">
+            <label htmlFor="firstNameInput">
+              <p>First Name</p>
+              <input type="text" name="firstName" id="firstNameInput" />
+            </label>
+
+            <label htmlFor="lastNameInput">
+              <p>Last Name</p>
+              <input type="text" name="lastName" id="lastNameInput" />
+            </label>
+
+            <label htmlFor="emailInput">
+              <p>Email</p>
+              <input type="text" name="email" id="emailInput" />
+            </label>
+
+            <label htmlFor="emailInput">
+              <p>Password</p>
+              <input type="password" name="password" id="passwordInput" />
+            </label>
+
+            <div className="formButtons">
+              <button type="button" onClick={() => setIsRegistering(false)}>
+                Login
+              </button>
+              <button type="submit" onClick={() => {}}>
+                Register
+              </button>
+            </div>
+          </form>
+        </section>
+      )}
+    </main>
   );
 };
